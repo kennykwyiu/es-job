85f09d795adeba706e1f3809271002ed850b3ee8
kenny <kenny.kwyiu@gmail.com>
2025-04-22 22:25:22 +0800

    TraceJobConfiguration

--- src/main/java/com/kenny/esjob/config/TraceJobConfiguration.java
+++ src/main/java/com/kenny/esjob/config/TraceJobConfiguration.java
@@ -1,4 +1,12 @@
 package com.kenny.esjob.config;
 
+import com.kenny.esjob.annotation.JobTraceInterceptor;
+import org.springframework.context.annotation.Bean;
+
 public class TraceJobConfiguration {
+    @Bean
+    public JobTraceInterceptor jobTraceInterceptor() {
+        System.err.println("init --------------->");
+        return new JobTraceInterceptor();
+    }
 }
