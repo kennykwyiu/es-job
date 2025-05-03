6716dd4a6c52f9a7fb2d1bba43d05ce593b5467d
kenny <kenny.kwyiu@gmail.com>
2025-05-03 21:30:02 +0800

    MySimpleJobConfig

--- src/main/java/com/kenny/esjob/config/MySimpleJobConfig.java
+++ src/main/java/com/kenny/esjob/config/MySimpleJobConfig.java
@@ -1,8 +1,11 @@
 package com.kenny.esjob.config;
 
+import com.dangdang.ddframe.job.api.simple.SimpleJob;
 import com.dangdang.ddframe.job.event.JobEventConfiguration;
 import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperRegistryCenter;
+import com.kenny.esjob.task.MySimpleJob;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 
 public class MySimpleJobConfig {
     @Autowired
@@ -10,4 +13,9 @@ public class MySimpleJobConfig {
 
     @Autowired
     private JobEventConfiguration jobEventConfiguration;
+
+    @Bean
+    public SimpleJob simpleJob() {
+        return new MySimpleJob();
+    }
 }
