b1e43dfd4d2b58748defe900150ab055c8ee5111
kenny <kenny.kwyiu@gmail.com>
2025-05-04 21:25:08 +0800

    MySimpleJobConfig

--- src/main/java/com/kenny/esjob/config/MySimpleJobConfig.java
+++ src/main/java/com/kenny/esjob/config/MySimpleJobConfig.java
@@ -1,10 +1,17 @@
 package com.kenny.esjob.config;
 
 import com.dangdang.ddframe.job.api.simple.SimpleJob;
+import com.dangdang.ddframe.job.config.JobCoreConfiguration;
+import com.dangdang.ddframe.job.config.simple.SimpleJobConfiguration;
 import com.dangdang.ddframe.job.event.JobEventConfiguration;
+import com.dangdang.ddframe.job.lite.api.JobScheduler;
+import com.dangdang.ddframe.job.lite.config.LiteJobConfiguration;
+import com.dangdang.ddframe.job.lite.spring.api.SpringJobScheduler;
 import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperRegistryCenter;
+import com.kenny.esjob.listener.SimpleJobListener;
 import com.kenny.esjob.task.MySimpleJob;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 
 public class MySimpleJobConfig {
@@ -17,5 +24,31 @@ public class MySimpleJobConfig {
     @Bean
     public SimpleJob simpleJob() {
         return new MySimpleJob();
+    }
+
+
+    private LiteJobConfiguration getLiteJobConfiguration(Class<? extends SimpleJob> jobClass, String cron,
+                                                         int shardingTotalCount, String shardingItemParameters, String jobParameter, boolean failover,
+                                                         boolean monitorExecution, int monitorPort, int maxTimeDiffSeconds, String jobShardingStrategyClass) {
+
+        JobCoreConfiguration jobCoreConfiguration = JobCoreConfiguration
+                .newBuilder(jobClass.getName(), cron, shardingTotalCount)
+                .misfire(true)
+                .failover(failover)
+                .jobParameter(jobParameter)
+                .shardingItemParameters(shardingItemParameters)
+                .build();
+
+        SimpleJobConfiguration simpleJobConfiguration = new SimpleJobConfiguration(jobCoreConfiguration, jobClass.getCanonicalName());
+
+        LiteJobConfiguration liteJobConfiguration = LiteJobConfiguration.newBuilder(simpleJobConfiguration)
+                .jobShardingStrategyClass(jobShardingStrategyClass)
+                .monitorExecution(monitorExecution)
+                .monitorPort(monitorPort)
+                .maxTimeDiffSeconds(maxTimeDiffSeconds)
+                .overwrite(false)
+                .build();
+
+        return liteJobConfiguration;
     }
 }
