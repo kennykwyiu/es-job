ed1222b97c91669201b534ed281358942da1f341
kenny <kenny.kwyiu@gmail.com>
2025-05-07 23:04:02 +0800

    RegistryCenterConfig

--- src/main/java/com/kenny/esjob/config/RegistryCenterConfig.java
+++ src/main/java/com/kenny/esjob/config/RegistryCenterConfig.java
@@ -1,9 +1,26 @@
 package com.kenny.esjob.config;
 
+import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperConfiguration;
+import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperRegistryCenter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConditionalOnExpression("'${zookeeper.address}'.length() > 0")
 public class RegistryCenterConfig {
+    @Bean(initMethod = "init")
+    public ZookeeperRegistryCenter registryCenter(@Value("${zookeeper.address}") final String serverLists,
+                                                  @Value("${zookeeper.namespace}") final String namespace,
+                                                  @Value("${zookeeper.connectionTimeout}") final int connectionTimeout,
+                                                  @Value("${zookeeper.sessionTimeout}") final int sessionTimeout,
+                                                  @Value("${zookeeper.maxRetries}") final int maxRetries) {
+        ZookeeperConfiguration zookeeperConfiguration = new ZookeeperConfiguration(serverLists, namespace);
+        zookeeperConfiguration.setConnectionTimeoutMilliseconds(connectionTimeout);
+        zookeeperConfiguration.setSessionTimeoutMilliseconds(sessionTimeout);
+        zookeeperConfiguration.setMaxRetries(maxRetries);
+
+        return new ZookeeperRegistryCenter(zookeeperConfiguration);
+    }
 }
