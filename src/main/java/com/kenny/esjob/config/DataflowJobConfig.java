4377ab1f0b40950478381aa8a928468a53f1df51
kenny <kenny.kwyiu@gmail.com>
2025-05-09 22:43:41 +0800

    DataflowJobConfig

--- src/main/java/com/kenny/esjob/config/DataflowJobConfig.java
+++ src/main/java/com/kenny/esjob/config/DataflowJobConfig.java
@@ -1,8 +1,11 @@
 package com.kenny.esjob.config;
 
+import com.dangdang.ddframe.job.api.dataflow.DataflowJob;
 import com.dangdang.ddframe.job.event.JobEventConfiguration;
 import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperRegistryCenter;
+import com.kenny.esjob.task.SpringDataflowJob;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 
 public class DataflowJobConfig {
     @Autowired
@@ -10,4 +13,9 @@ public class DataflowJobConfig {
 
     @Autowired
     private JobEventConfiguration jobEventConfiguration;
+
+    @Bean
+    public DataflowJob dataflowJob() {
+        return new SpringDataflowJob();
+    }
 }
