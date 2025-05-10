41acee057abc66dc3aa2e8569e6d3e7ad00d6a81
kenny <kenny.kwyiu@gmail.com>
2025-05-10 15:00:11 +0800

    DataflowJobConfig

--- src/main/java/com/kenny/esjob/config/DataflowJobConfig.java
+++ src/main/java/com/kenny/esjob/config/DataflowJobConfig.java
@@ -1,7 +1,10 @@
 package com.kenny.esjob.config;
 
 import com.dangdang.ddframe.job.api.dataflow.DataflowJob;
+import com.dangdang.ddframe.job.config.JobCoreConfiguration;
+import com.dangdang.ddframe.job.config.dataflow.DataflowJobConfiguration;
 import com.dangdang.ddframe.job.event.JobEventConfiguration;
+import com.dangdang.ddframe.job.lite.config.LiteJobConfiguration;
 import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperRegistryCenter;
 import com.kenny.esjob.task.SpringDataflowJob;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,5 +20,15 @@ public class DataflowJobConfig {
     @Bean
     public DataflowJob dataflowJob() {
         return new SpringDataflowJob();
+    }
+
+    private LiteJobConfiguration getLiteJobConfiguration(final Class<? extends DataflowJob> jobClass, final String cron, final int shardingTotalCount, final String shardingItemParameters) {
+        return LiteJobConfiguration.newBuilder(
+                        new DataflowJobConfiguration(JobCoreConfiguration.newBuilder(jobClass.getName(), cron, shardingTotalCount)
+                                .shardingItemParameters(shardingItemParameters).build(),
+                                jobClass.getCanonicalName(),
+                                false))	//streamingProcess
+                .overwrite(false)
+                .build();
     }
 }
